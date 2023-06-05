520e832d920e15eb727af6add11b215ef141bec3
nathannlw <135446453+nathannlw@users.noreply.github.com>
2023-06-05 16:54:34 +0700

    Create haloo.go

---
+++ haloo.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+var(
+  kabar string
+  )
+  fmt.Println("haloo tmen tmen nama gue nathann, apa kabar kaliann? ")
+  fmt.Scanln(&kabar)
+  if(kabar=="baik"){
+    fmt.Println("wahh mantapp")
+    } else {
+      fmt.Println("lohee kenapa we")
+      }
+    
